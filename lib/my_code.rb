b98c203ab7a4ecd4ba51dfe30686082a0fa54197
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-14 15:30:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,6 +5,7 @@ def map(source_array)
   while counter < source_array.length do 
     source_array[counter] * -1
     counter += 1
+  end
   new_array.push(source_array.map
   yield(new_array)
   new_array
