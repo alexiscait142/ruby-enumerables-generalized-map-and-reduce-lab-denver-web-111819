6a33ffbb4c7bb3732864cccbe14dbc3c61648e6f
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-14 15:28:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,7 @@
 # Your Code Here
 def map(source_array)
-  new_array = source_array.map{|n| n * -1}
+  new_array = []
+  new_array.push(source_array.map{|n| n * -1})
   yield(new_array)
   new_array
 end
